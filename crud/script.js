be3bf7ef0d91859ac484e565dd97a3f3d117c0fe
Ceu200317 <ib.marcelle.m@gmail.com>
2025-06-30 13:33:17 -0300

    Update script.js

--- crud/script.js
+++ crud/script.js
@@ -1,35 +1,88 @@
-function abrirModal(id) {
+
+async function abrirModal(id) {
     document.getElementById('modalEditar').style.display = 'flex';
-    carregarUmaTarefa(id)
-    console.log(id)
+    const tarefa = await carregarUmaTarefaSo(id)
+    const inputId = document.querySelector('#editar-id')
+    const inputDescricao = document.querySelector('#editar-descricao')
+    inputId.value = tarefa.id
+    inputDescricao.value = tarefa.descricao
 }
 function fecharModal() {
     document.getElementById('modalEditar').style.display = 'none';
 }
 
-async function carregarUmaTarefa(id) {
-    const url = `http://localhost:3000/tarefas/${id}`
-    const resposta = await fetch(url)
-    const tarefa = await resposta.json()   
-    console.log(tarefa)
+async function carregarTarefas() {
+    const url = 'http://localhost:3000/tarefas'
+    const response = await fetch(url)
+    const tarefas = await response.json()
+
+    const listaTarefas = document.querySelector('#lista-tarefas')
+
+    listaTarefas.innerHTML = ''
+
+    tarefas.forEach(tarefa => {
+        listaTarefas.innerHTML += `      
+            <div class="tarefa" data-id="${tarefa.id}">
+                <span class="texto-tarefa">${tarefa.descricao}</span>
+                <div class="botoes-tarefa">
+                    <button class="botao-editar" onclick="abrirModal('${tarefa.id}')">Editar</button>
+                    <button class="botao-deletar" onclick="deletarTarefa('${tarefa.id}')">Deletar</button>
+                </div>
+            </div>` 
+    })
 }
 
-async function carregarTarefas(params) {
+
+async function deletarTarefa(id) {
+    const url = `http://localhost:3000/tarefas/${id}`
+    console.log(id)
+    const response = await fetch(url, {
+        method: 'DELETE'
+    })
+}
+
+async function adicionarTarefa() {
     const url = 'http://localhost:3000/tarefas'
-    const resposta = await fetch(url)
-    const tarefas = await resposta.json()
-    const divListaTarefas = document.getElementById('lista-tarefas')
-    divListaTarefas.innerHTML = ''
-    tarefas.forEach(tarefa => {
-       divListaTarefas.innerHTML += 
-       `<div class="tarefa" >
-        <span class="texto-tarefa">${tarefa.descricao}</span>
-        <div class="botoes-tarefa">
-          <button class="botao-editar" onclick="abrirModal('${tarefa.id}')">Editar</button>
-          <button class="botao-deletar">Deletar</button>
-        </div>
-      </div>`
-    });  
+
+    const descricaoTarefa = document.querySelector('#entradaNovaTarefa').value
+
+    const novaTarefa = {
+        "descricao": descricaoTarefa
+    }
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(novaTarefa)
+    })
+}
+
+
+async function carregarUmaTarefaSo(id) {
+    const url = `http://localhost:3000/tarefas/${id}`
+    const response = await fetch(url)
+    const tarefa = await response.json()
+    return tarefa
+}
+
+async function alterarTarefa() {
+    const idDigitado = document.querySelector('#editar-id').value
+    const descricaoDigitado = document.querySelector('#editar-descricao').value
+    
+    const url = `http://localhost:3000/tarefas/${idDigitado}`
+    
+    const dadosTarefa = {
+        id: idDigitado,
+        descricao: descricaoDigitado 
+    }
+
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dadosTarefa)
+    })
+
+    
 }
 
 carregarTarefas()
