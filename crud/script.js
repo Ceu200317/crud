cf70f1fd57ad6f28dfbc040beb47cab143e30c05
Ceu200317 <ib.marcelle.m@gmail.com>
2025-07-07 13:02:30 -0300

    Update script.js

--- crud/script.js
+++ crud/script.js
@@ -1,24 +1,23 @@
-
 async function abrirModal(id) {
     document.getElementById('modalEditar').style.display = 'flex';
-    const tarefa = await carregarUmaTarefaSo(id)
-    const inputId = document.querySelector('#editar-id')
-    const inputDescricao = document.querySelector('#editar-descricao')
-    inputId.value = tarefa.id
-    inputDescricao.value = tarefa.descricao
+    const tarefa = await carregarUmaTarefaSo(id);
+    const inputId = document.querySelector('#editar-id');
+    const inputDescricao = document.querySelector('#editar-descricao');
+    inputId.value = tarefa.id;
+    inputDescricao.value = tarefa.descricao;
 }
+
 function fecharModal() {
     document.getElementById('modalEditar').style.display = 'none';
 }
 
 async function carregarTarefas() {
-    const url = 'http://localhost:3000/tarefas'
-    const response = await fetch(url)
-    const tarefas = await response.json()
+    const url = 'http://localhost:3000/tarefas';
+    const response = await fetch(url);
+    const tarefas = await response.json();
 
-    const listaTarefas = document.querySelector('#lista-tarefas')
-
-    listaTarefas.innerHTML = ''
+    const listaTarefas = document.querySelector('#lista-tarefas');
+    listaTarefas.innerHTML = '';
 
     tarefas.forEach(tarefa => {
         listaTarefas.innerHTML += `      
@@ -29,60 +28,66 @@ async function carregarTarefas() {
                     <button class="botao-deletar" onclick="deletarTarefa('${tarefa.id}')">Deletar</button>
                 </div>
             </div>` 
-    })
+    });
 }
 
-
 async function deletarTarefa(id) {
-    const url = `http://localhost:3000/tarefas/${id}`
-    console.log(id)
-    const response = await fetch(url, {
+    const url = `http://localhost:3000/tarefas/${id}`;
+    await fetch(url, {
         method: 'DELETE'
-    })
+    });
+    carregarTarefas(); 
 }
 
 async function adicionarTarefa() {
-    const url = 'http://localhost:3000/tarefas'
+    const url = 'http://localhost:3000/tarefas';
+    const input = document.querySelector('#entradaNovaTarefa');
+    const descricaoTarefa = input.value.trim();
 
-    const descricaoTarefa = document.querySelector('#entradaNovaTarefa').value
+    if (!descricaoTarefa) return; 
 
     const novaTarefa = {
-        "descricao": descricaoTarefa
-    }
+        descricao: descricaoTarefa
+    };
 
-    const response = await fetch(url, {
+    await fetch(url, {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(novaTarefa)
-    })
+    });
+
+    input.value = ''; 
+    carregarTarefas(); 
 }
 
-
 async function carregarUmaTarefaSo(id) {
-    const url = `http://localhost:3000/tarefas/${id}`
-    const response = await fetch(url)
-    const tarefa = await response.json()
-    return tarefa
+    const url = `http://localhost:3000/tarefas/${id}`;
+    const response = await fetch(url);
+    const tarefa = await response.json();
+    return tarefa;
 }
 
 async function alterarTarefa() {
-    const idDigitado = document.querySelector('#editar-id').value
-    const descricaoDigitado = document.querySelector('#editar-descricao').value
-    
-    const url = `http://localhost:3000/tarefas/${idDigitado}`
-    
+    const idDigitado = document.querySelector('#editar-id').value;
+    const descricaoDigitado = document.querySelector('#editar-descricao').value.trim();
+
+    if (!descricaoDigitado) return; 
+
+    const url = `http://localhost:3000/tarefas/${idDigitado}`;
     const dadosTarefa = {
         id: idDigitado,
         descricao: descricaoDigitado 
-    }
+    };
 
-    const response = await fetch(url, {
+    await fetch(url, {
         method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(dadosTarefa)
-    })
+    });
 
-    
+    fecharModal();       
+    carregarTarefas();   
 }
 
-carregarTarefas()
+
+carregarTarefas();
